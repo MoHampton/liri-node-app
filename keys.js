6d674bdd825d32d1376eb0e5a54346349b229b78
Monique Hampton <moniquejhampton@yahoo.com>
2017-07-26 22:10:01 -0400

    twitter keys

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'Puvk2aHceIADWfZeu3TGafnMW',
+  consumer_secret: '67CNQc4RLHRL0Dq9qPgr4LWq6wpc8UIB3DH3QCCLjoMJzvM6Re',
+  access_token_key: '890292961409368065-bX3gyiwkAAe2kcNOOMlr1CIDY0vajgy',
+  access_token_secret: 'q5mjjGJroOcYOQ8bvJDw0hn37o4Ot3m1toEK1s1FhJfUD',
 }
