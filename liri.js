be3b184fe0fc8be9acdd035d3fbb2c55441257c8
Monique Hampton <moniquejhampton@yahoo.com>
2017-07-27 00:12:55 -0400

    twitter function

--- liri.js
+++ liri.js
@@ -1,7 +1,27 @@
+//global variables
 var userCommand = process.argv[2];
 var userInput = process.argv[3];
+var fs = require('fs');
 
-//twitter
+//twitter keys
 var Twitter = require('twitter');
 var keys = require('./keys.js');
 var client = new Twitter(keys.twitterKeys);
+var params = {screen_name: 'atomic_covert', count: 20};
+
+//twitter function
+function myTweets(){
+        client.get('statuses/user_timeline', params, function(error, tweets, response) {
+            if (!error && response.statusCode == 200) {
+                // console.log(tweets);
+                for(var i = 0; i < tweets.length; i++) {
+                        console.log("\n============== Tweet " + [i] + " =============\n");
+                        console.log(tweets[i].text);
+                        // console.log(tweets);
+                        ;
+                }//end of twitter for loop
+            }//end of conditional statement
+        });
+    }; // end twitter function
+
+myTweets(); 
