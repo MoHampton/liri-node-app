f1b6f8a6c8fa9ef9713a1b17e3bc08964555be79
Monique Hampton <moniquejhampton@yahoo.com>
2017-07-27 01:56:00 -0400

    spotify function

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '890292961409368065-bX3gyiwkAAe2kcNOOMlr1CIDY0vajgy',
   access_token_secret: 'q5mjjGJroOcYOQ8bvJDw0hn37o4Ot3m1toEK1s1FhJfUD',
 }
+
+exports.spotifyKeys = {
+  id: '5ca6207dbdb847c681cb8ad0442e5a56',
+  secret: 'e7a55a1a08094e7f92e5a25db6ab832d',
+}
--- liri.js
+++ liri.js
@@ -3,7 +3,20 @@ var userCommand = process.argv[2];
 var userInput = process.argv[3];
 var fs = require('fs');
 
-//twitter keys
+
+
+
+//JS switch
+switch(userCommand) {
+  case "my-tweets":
+    myTweets();
+    break;
+  case "spotify-this-song":
+    spotifyThis();
+    break;
+}
+
+//twitter variables
 var Twitter = require('twitter');
 var keys = require('./keys.js');
 var client = new Twitter(keys.twitterKeys);
@@ -17,11 +30,40 @@ function myTweets(){
                 for(var i = 0; i < tweets.length; i++) {
                         console.log("\n============== Tweet " + [i] + " =============\n");
                         console.log(tweets[i].text);
-                        // console.log(tweets);
-                        ;
                 }//end of twitter for loop
             }//end of conditional statement
         });
     }; // end twitter function
 
-myTweets(); 
+//spotify variables
+ var spotify = require('node-spotify-api')
+ var spotifyApi = new spotify(keys.spotifyKeys);
+
+
+//spotify function
+	function spotifyThis() {
+    if (userInput != undefined) {
+		spotifyApi.search({type: 'track', query: userInput + '&limit=5'}, function (err,data) {
+        if (err) {
+          return console.log(err);
+        } else {
+          if (data.tracks.items.length === 0) {
+            return console.log('Track not found, please try again');
+          }
+          // console.log(data);
+          console.log('Artist:', data.tracks.items[0].artists[0].name);
+          console.log('Track:', data.tracks.items[0].name);
+          console.log('Preview Link:', data.tracks.items[0].preview_url);
+          console.log('Album:', data.tracks.items[0].album.name);
+          console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++');
+        }
+        });
+
+  	} else {
+        userInput = 'The Sign';
+  	}
+}//end spotify function
+
+
+spotifyThis();
+myTweets();
